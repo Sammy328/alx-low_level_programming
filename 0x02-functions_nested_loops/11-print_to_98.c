64164a48449b14a5a7a31f14fb4eab2550efa06a
Sammy328 <samanthandlovu36@gmail.com>
2023-07-19 18:18:54 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to-98 - prints all natural numbers from n to 98
+ * print_to_98 - prints all natural numbers from n to 98
  *
  * @n: starting point
  *

c2c4a41bedb9930ee1f753e30dc9e6496609e30e
Sammy328 <samanthandlovu36@gmail.com>
2023-07-27 15:42:13 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,11 +7,10 @@
  * Return: void
  */
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 int a = -1, i;
 for (i = 0; dest[i] != '\0'; i++);
-
 do {
 	a++;
 	dest[i] = src[a];
